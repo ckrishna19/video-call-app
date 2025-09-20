fbf2b477589d68e527c01ad5e0d33bb6033edb44
Sirish Chalise <chalise.krishna19@gmail.com>
2025-09-20 20:01:25 -0230

    try some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
-import { lazy, Suspense, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PrivateRoute from "./utils/PrivateRoute";
-import PublicRoute from "./utils/PublicRoute";
-import "./App.css";
-const Home = lazy(() => import("./pages/Home"));
-const Login = lazy(() => import("./pages/Login"));
-const Register = lazy(() => import("./pages/Register"));
-const ForgetPassword = lazy(() => import("./pages/ForgetPassword"));
-function App() {
+// import { lazy, Suspense, useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import PrivateRoute from "./utils/PrivateRoute";
+// import PublicRoute from "./utils/PublicRoute";
+// import "./App.css";
+// const Home = lazy(() => import("./pages/Home"));
+// const Login = lazy(() => import("./pages/Login"));
+// const Register = lazy(() => import("./pages/Register"));
+// const ForgetPassword = lazy(() => import("./pages/ForgetPassword"));
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Suspense fallback={<p>Loading...</p>}>
+//         <Routes>
+//           <Route element={<PublicRoute />}>
+//             <Route path="/register" element={<Register />} />
+//             <Route path="/login" element={<Login />} />
+//             <Route path="/forget-password" element={<ForgetPassword />} />
+//           </Route>
+//           <Route element={<PrivateRoute />}>
+//             <Route path="/" element={<Home />} />
+//           </Route>
+//         </Routes>
+//       </Suspense>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+
+const App = () => {
   return (
-    <BrowserRouter>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Routes>
-          <Route element={<PublicRoute />}>
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/forget-password" element={<ForgetPassword />} />
-          </Route>
-          <Route element={<PrivateRoute />}>
-            <Route path="/" element={<Home />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <div>
+      <p>app</p>
+    </div>
   );
-}
+};
 
 export default App;
